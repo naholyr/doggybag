6c5980998535ec091d91ed66e4dc4e1d31d41d52
Nicolas Chambrier <naholyr@gmail.com>
2012-06-21 15:59:59 +0200

    Add 'logger'

---
+++ logger/index.js
@@ -0,0 +1,44 @@
+/**
+ * Generic logger configured depending on env.
+ */
+
+'use strict';
+
+var winston = require('winston');
+
+var loggers = module.exports = {};
+
+loggers.add = function add(names, config) {
+  // Default config = embedded configuration handler using "logging" category
+  if (!config) {
+    config = require('../config').logging;
+  }
+  // Retrieve a config part: supports hash or nconf-styled ".get"
+  var getConfig = function (name) {
+    if (typeof config.get === 'function') {
+      return config.get(name);
+    } else {
+      return config[name];
+    }
+  };
+  // names can be a single name
+  if (!Array.isArray(names)) {
+    names = [names];
+  }
+  // Reserved name
+  if (~names.indexOf('add')) {
+    throw new Error("'add' can't be used as a logger name, you could also divide by zero if you want to break the universe");
+  }
+  // Initialize loggers
+  var initializedLoggers = [];
+  names.forEach(function (name) {
+    loggers[name] = winston.loggers.add(name, getConfig(name));
+    initializedLoggers.push(loggers[name]);
+  });
+  // Return initialized loggers, keeping homogeneity with parameters format
+  if (initializedLoggers.length === 1) {
+    return initializedLoggers[0];
+  } else {
+    return initializedLoggers;
+  }
+};
